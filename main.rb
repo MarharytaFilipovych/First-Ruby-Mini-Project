5e886784a2c91e2d17c7194df503faea44dd44de
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2024-11-04 22:50:09 +0200

    fixed sth in student class and added unit test

--- main.rb
+++ main.rb
@@ -9,7 +9,6 @@ class Student
     @name = name
     @surname = surname
     @date = date
-    add_student
 
   end
   def self.all
@@ -29,21 +28,22 @@ class Student
       puts "No students yet"
     else
       @@all.each { |student|  student.display }
-
     end
   end
   def unique?
-    @@all.none?{|student| student.name == @name && student.surname == @surname && @date == self.date}
+    !self.class.all.any? { |student| student.name == @name && student.surname == @surname && student.date == @date }
   end
 
-  def add_student
-    if unique?
-      @@all << self
-      puts "#{@name} #{@surname}, who was born on #{@date} was been added"
+
+
+  def self.add_student(student)
+    if student.unique?
+      @@all << student
     else
-      puts "#{@name} #{@surname}, who was born on #{@date} was added previously!"
+      puts "This student already exists."
     end
   end
+
   def get_given_date(date)
     day, month, year = date.split('-').map(&:to_i)
     Time.new(year, month, day)
@@ -57,16 +57,15 @@ class Student
 
   def calculate_age
     given_date = get_given_date(@date)
-    ((Time.now - given_date) / (60*60*24*365)).to_i
+    age = Time.now.year - given_date.year
+    if (given_date.month > Time.now.month) || (given_date.day > Time.now.day && given_date.month == Time.now.month)then age -= 1 end
+    age
   end
 
   def self.remove_student(student)
     index = @@all.find_index { |s| s.name == student.name && s.surname == student.surname && s.date == student.date }
     if index
       @@all.delete_at(index)
-      puts "#{student.name} #{student.surname}, who was born on #{student.date} was removed."
-    else
-      puts "Student is not here."
     end
   end
 
@@ -78,36 +77,3 @@ class Student
     @@all.select{|student| student.name == name}
   end
 end
-
-
-student1 = Student.new("Mary", "Fox", "03-04-2004")
-student2 = Student.new("Kate", "Smith", "29-09-2017")
-student4 = Student.new("Borys", "Lake", "01-01-2001")
-student5 = Student.new("Mary", "Fox", "03-04-2004")
-student6 = Student.new("Kate", "Smith", "29-09-2017")
-
-puts "Student Info for Mary:"
-student1.display
-
-puts "Before deleting:"
-Student.display_all_students_info
-
-Student.remove_student(student1)
-
-puts "After deleting:"
-Student.display_all_students_info
-
-puts "Searching for students named Kate:"
-puts Student.get_student_by_name("Kate").map(&:display)
-
-puts "Searching for students named '...':"
-puts Student.get_student_by_name("...").map(&:display)
-
-puts "Students aged 13:"
-puts Student.get_students_by_age(13).map(&:display)
-
-puts "Students aged 20:"
-puts Student.get_students_by_age(20).map(&:display)
-
-puts "Students aged #{student2.calculate_age}:"
-puts Student.get_students_by_age(student2.calculate_age).map(&:display)
---
+++ tests/unit.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require_relative '../main'
+
+Minitest::Reporters.use!
+  Minitest::Reporters::HtmlReporter.new(
+    reports_dir: 'test',
+    report_filename: 'tests_results.html',
+    add_timestamp: true
+  )
+
+
+class MyTest < Minitest::Test
+
+  def setup
+    Student.all.clear
+    @student = Student.new("Kira", "Smith", "12-12-2010")
+    Student.add_student(@student)
+  end
+
+  def test_initialize
+    assert_equal "Kira", @student.name
+    assert_equal "Smith", @student.surname
+    assert_equal "12-12-2010", @student.date
+  end
+
+
+  def test_add_student
+    assert_includes Student.all, @student
+  end
+
+  def test_calculate_age
+    age = @student.calculate_age
+    assert_equal 13, age
+  end
+
+  def test_unique
+    duplicate = Student.new("Kira", "Smith", "12-12-2010")
+    refute duplicate.unique?, "Expected the duplicate student to not be unique"
+  end
+
+  def test_display
+    assert_output("Kira Smith, born on 12-12-2010\n") {@student.display}
+  end
+  def test_get_by_age
+    students = Student.get_students_by_age(@student.calculate_age)
+    assert_includes students, @student
+  end
+
+  def test_get_student_by_name
+    students = Student.get_student_by_name("Kira")
+    assert_includes students, @student
+  end
+
+  def test_remove
+    Student.remove_student(@student)
+    refute_includes Student.all, @student
+  end
+
+  def teardown
+    Student.all.clear
+  end
+end
